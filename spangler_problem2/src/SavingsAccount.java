8c1fb33cde0091f742ecf70f9c1ba58423a52b33
Brandon  Spangler <brandonspangler@Brandons-MacBook-Pro.local>
2020-02-22 14:14:37 -0500

    savings account finished

--- spangler_problem2/src/SavingsAccount.java
+++ spangler_problem2/src/SavingsAccount.java
@@ -1,4 +1,25 @@
 
 public class SavingsAccount {
 
+	private static double annualInterestRate;
+	
+	private double savingsBalance;
+	
+	public SavingsAccount(double savingsBalance) {
+		this.savingsBalance = savingsBalance;
+	}
+	
+	public void calculateMonthlyInterest() {
+		savingsBalance += (savingsBalance * annualInterestRate)/12;
+	}
+	
+	public static void modifyInterestRate(double aIR) {
+		annualInterestRate = aIR;
+	}
+	
+	public double getSavingsBalance() {
+		return this.savingsBalance;
+	}
+	
+	
 }
--- spangler_problem2/src/SavingsAccountTest.java
+++ spangler_problem2/src/SavingsAccountTest.java
@@ -1,4 +1,38 @@
+import java.text.DecimalFormat;
 
 public class SavingsAccountTest {
-
+	
+	private static DecimalFormat df = new DecimalFormat("0.00");
+	
+	public static void main(String[] args) {
+		
+		final int year = 12;
+		
+		SavingsAccount saver1 = new SavingsAccount(2000);
+		SavingsAccount saver2 = new SavingsAccount(3000);
+		
+		SavingsAccount.modifyInterestRate(0.04);
+		
+		for (int i = 0; i < year; i++) {
+			saver1.calculateMonthlyInterest();
+			saver2.calculateMonthlyInterest();
+		}
+		
+		System.out.println("Savings Balance: " + df.format(saver1.getSavingsBalance()));
+		
+		System.out.println("Savings Balance: " + df.format(saver2.getSavingsBalance()));
+		
+		SavingsAccount.modifyInterestRate(0.05);
+		
+		saver1.calculateMonthlyInterest();
+		saver2.calculateMonthlyInterest();
+		
+		System.out.println("Savings Balance: " + df.format(saver1.getSavingsBalance()));
+		
+		System.out.println("Savings Balance: " + df.format(saver2.getSavingsBalance()));
+		
+		
+		
+	}
+	
 }
